3fe57437d898f3e3dd069215b3d4a22f68da3332
William Braney <williambraney@aol.com>
2021-02-08 15:59:56 +0000

    moved into day_1 directory

---
+++ day_1/basic_php/06-for.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+$total = 0;
+
+for($i = 1; $i < 51; $i += 1){
+    $total = $total += $i;
+    dump($total);
+}
+
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+$total = 0;
+
+for($i = 1; $i < 51; $i += 1){
+    $total = $total += $i;
+    dump($total);
+}
