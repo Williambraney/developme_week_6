8d373349eb191955597fb561dd9371279d09b8c3
William Braney <williambraney@aol.com>
2021-02-08 16:20:38 +0000

    renamed folder

--- day_1/basic_php/06-for.php
+++ day_1/basic_php/06-for.php
@@ -8,14 +8,3 @@ for($i = 1; $i < 51; $i += 1){
     $total = $total += $i;
     dump($total);
 }
-
-<?php
-
-require __DIR__ . "/vendor/autoload.php";
-
-$total = 0;
-
-for($i = 1; $i < 51; $i += 1){
-    $total = $total += $i;
-    dump($total);
-}
